2222b52b418f8942fde0f0ea1ccf82ff62b653c9
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2025-01-11 15:12:59 +0000

    selectBox function created

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
-const board = [X, -1, -1, -1, -1, -1, -1, -1, -1];
+const board = [-1,-1, -1, -1, -1,-1, -1, -1, -1];
+let currentPlayer = 'X';
 
 function renderBoard() {
     const allBoxes = document.querySelectorAll('.box');
 
     allBoxes.forEach((boxElement, i) => {
         if (board[i] === 'X') {
-            boxElement.innerHTML = '<img src=".IMG/player1.png" height="50px" />';
+            boxElement.innerHTML = '<img src="player1.png" height="70px" />';
         }
         else if (board[i] === 'O') {
-            boxElement.innerHTML = '<img src=".IMG/player2.png" height="50px" />';
+            boxElement.innerHTML = '<img src="player2.png" height="70px" />';
         }
         else {
             boxElement.innerHTML = '';
@@ -17,3 +18,19 @@ function renderBoard() {
 
 }
 renderBoard();
+
+
+function selectBox(boxNumber){
+  board[boxNumber] = currentPlayer;
+  renderBoard();
+
+  if(currentPlayer === "X"){
+    currentPlayer = "O";
+  }
+  else{
+    currentPlayer = "X";
+  }
+
+  const currentPlayerElement = document.getElementById("currentPlayer");
+  currentPlayerElement.innerText = currentPlayer;
+}
