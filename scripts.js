93150e36d3aea3444bb5b597d125492609bfebf7
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2025-01-12 06:25:37 +0000

    CheckforWinner() Function created

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const board = [-1,-1, -1, -1, -1,-1, -1, -1, -1];
+const board = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 let currentPlayer = 'X';
 
 function renderBoard() {
@@ -19,24 +19,74 @@ function renderBoard() {
 renderBoard();
 
 
-function selectBox(boxNumber){
-    if(board[boxNumber] !== -1){
-        alert(" Invalid Move : : Select Unfilled Box");
+function selectBox(boxNumber) {
+    if (board[boxNumber] !== -1) {
+        alert(" Invalid Move : : Select Unfilled Boxes");
         return;
     }
 
+    board[boxNumber] = currentPlayer;
+    renderBoard();
 
-  board[boxNumber] = currentPlayer;
-  renderBoard();
+    if (currentPlayer === "X") {
+        currentPlayer = "O";
+    }
+    else {
+        currentPlayer = "X";
+    }
 
-  if(currentPlayer === "X"){
-    currentPlayer = "O";
-  }
-  else{
-    currentPlayer = "X";
-  }
-
-  const currentPlayerElement = document.getElementById("currentPlayer");
-  currentPlayerElement.innerText = currentPlayer;
+    const currentPlayerElement = document.getElementById("currentPlayer");
+    currentPlayerElement.innerText = currentPlayer;
 }
 
+
+function resetBoard() {
+    board.fill(-1);
+    renderBoard();
+
+    currentPlayer = "X";
+    const currentPlayerElement = document.getElementById("currentPlayer");
+    currentPlayerElement.innerText = currentPlayer;
+}
+
+function isPlayerPresent(places, player) {
+
+    const a = places[0];
+    const b = places[1];
+    const c = places[2];
+
+    if (board[a] == player && board[b] == player && board[c] == player) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+function checkForWinner() {
+    if (isPlayerPresent ([0,1,2], "X")) {
+        alert('Player X Wins 🎉');
+    }
+    else if (isPlayerPresent ([3,4,5], "X")) {
+        alert('Player X Wins 🎉');
+    }
+    else if (isPlayerPresent ([6,7,8], "X")) {
+        alert('Player X Wins 🎉');
+    }
+    else if (isPlayerPresent ([0,3,6], "X")) {
+        alert('Player X Wins 🎉');
+    }
+    else if (isPlayerPresent ([1,4,7], "X")) {
+        alert('Player X Wins 🎉');
+    }
+    else if (isPlayerPresent ([2,5,8], "X")) {
+        alert('Player X Wins 🎉');
+    }
+    else if (isPlayerPresent ([0,4,8], "X")) {
+        alert('Player X Wins 🎉');
+    }
+    else if (isPlayerPresent ([2,4,6], "X")) {
+        alert('Player X Wins 🎉');
+    }
+}
